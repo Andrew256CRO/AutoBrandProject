9267489e33047fe0f92cb9830254ea156605a117
Andrei Boclaci <e100is7@yahoo.com>
2026-05-23 15:13:59 +0300

    last change

--- pdfcontroller.php
+++ pdfcontroller.php
@@ -50,38 +50,62 @@ function extractProductsFromInvoice($text)
 {
     $products = [];
 
-    // Curatam textul
+    // Curatam textul - inlocuim newlines cu spatiu
     $text = preg_replace('/\s+/', ' ', $text);
 
-    // Pattern pentru linia de produs din factura:
-    // Format: [numar_linie] [cod_produs] [denumire] [pret] [moneda] [cantitate]
-    // Ex: "1 172812F COMUTATOR PORNIRE FEBI 251.96 RON -1"
-    $pattern = '/(\d+)\s+([A-Z0-9]+)\s+([A-Z][A-Z0-9\s]+?)\s+([\d]+\.[\d]{2})\s+(RON|EUR|USD)\s+(-?\d+)/i';
+    // Din textul brut, linia produsului arata asa:
+    // "251.96 RON -1    -1 H87 19 -251.96172812F COMUTATOR PORNIRE FEBI1 172812F"
+    // Deci: [pret] [moneda] [cantitate] ... [cod_produs][denumire][linie_nr] [cod_produs]
+
+    // Pattern: pret RON/EUR/USD cantitate ... cod denumire
+    // Codul e format din litere si cifre lipit de denumire: "172812F COMUTATOR PORNIRE FEBI"
+    $pattern = '/([\d]+\.[\d]{2})\s+(RON|EUR|USD)\s+(-?\d+)[\s\S]*?(-?\d+)\s+[A-Z0-9]+\s+\d+\s+[-\d.]+([A-Z0-9]{5,})\s+([A-Z][A-Z0-9\s]+?)\d+\s+\5/';
 
     if (preg_match_all($pattern, $text, $matches, PREG_SET_ORDER)) {
         foreach ($matches as $match) {
             $products[] = [
-                'cod_produs' => trim($match[2]),
-                'denumire' => trim($match[3]),
-                'pret_unitar' => (float) $match[4],
-                'moneda' => strtoupper($match[5]),
-                'cantitate' => (int) $match[6]
+                'cod_produs' => trim($match[5]),
+                'denumire' => trim($match[6]),
+                'pret_unitar' => (float) $match[1],
+                'moneda' => $match[2],
+                'cantitate' => (int) $match[3]
             ];
         }
     }
 
-    // Daca pattern-ul principal nu gaseste nimic, incercam alternativ
+    // Daca nu gaseste cu pattern complex, folosim pattern simplu direct pe date cunoscute
     if (empty($products)) {
-        // Cautam codul de produs cunoscut si extragem contextul
-        $altPattern = '/([A-Z0-9]{5,})\s+([A-Z][A-Z\s]+?)\s+([\d]+\.[\d]{2})\s+(RON|EUR|USD)\s+(-?\d+)/i';
-        if (preg_match_all($altPattern, $text, $matches, PREG_SET_ORDER)) {
+        // Pattern simplificat: cauta [pret] [RON/EUR/USD] [cantitate negativa sau pozitiva]
+        // si [cod alfanumeric] [denumire majuscule]
+        $simplePattern = '/([\d]+\.[\d]{2})\s+(RON|EUR|USD)\s+(-?\d+)\s+.*?([A-Z0-9]{5,})\s+([A-Z][A-Z\s]+?)(?=\d|\s*[A-Z0-9]{5,}|\s*Identificator)/';
+
+        if (preg_match_all($simplePattern, $text, $matches, PREG_SET_ORDER)) {
             foreach ($matches as $match) {
                 $products[] = [
-                    'cod_produs' => trim($match[1]),
-                    'denumire' => trim($match[2]),
-                    'pret_unitar' => (float) $match[3],
-                    'moneda' => strtoupper($match[4]),
-                    'cantitate' => (int) $match[5]
+                    'cod_produs' => trim($match[4]),
+                    'denumire' => trim($match[5]),
+                    'pret_unitar' => (float) $match[1],
+                    'moneda' => $match[2],
+                    'cantitate' => (int) $match[3]
+                ];
+            }
+        }
+    }
+
+    // Fallback - hardcodat pe structura exacta a acestei facturi
+    if (empty($products)) {
+        // Stim exact ce contine factura din debug
+        // "251.96 RON -1 -1 H87 19 -251.96172812F COMUTATOR PORNIRE FEBI1 172812F"
+        $fallbackPattern = '/([\d]+\.[\d]{2})\s+(RON|EUR|USD)\s+(-?\d+).*?([\d]+[A-Z]+|[A-Z]+[\d]+[A-Z]*)\s+([A-Z][A-Z0-9\s]+?)(?=\d+\s+\4|Identificator)/';
+
+        if (preg_match_all($fallbackPattern, $text, $matches, PREG_SET_ORDER)) {
+            foreach ($matches as $match) {
+                $products[] = [
+                    'cod_produs' => trim($match[4]),
+                    'denumire' => trim($match[5]),
+                    'pret_unitar' => (float) $match[1],
+                    'moneda' => $match[2],
+                    'cantitate' => (int) $match[3]
                 ];
             }
         }
